f12564dcf42e380035c1cec6358556af542be9ab
box7012 <box7012@naver.com>
2025-02-01 21:28:10 +0900

    code added

--- src/main/java/app/message/demo1/SparkConfig.java
+++ src/main/java/app/message/demo1/SparkConfig.java
@@ -1,19 +1,19 @@
-package app.message.demo1;
+// package app.message.demo1;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-import org.apache.spark.sql.SparkSession;
+// import org.apache.spark.sql.SparkSession;
 
-@Configuration
-public class SparkConfig {
+// @Configuration
+// public class SparkConfig {
 
-    @Bean
-    public SparkSession sparkSession() {
-        return SparkSession.builder()
-                .appName("PortfolioOptimization")
-                .master("local[*]")  // 로컬 실행 (클러스터에서는 변경)
-                .config("spark.sql.shuffle.partitions", "4") // 설정 추가 가능
-                .getOrCreate();
-    }
-}
+//     @Bean
+//     public SparkSession sparkSession() {
+//         return SparkSession.builder()
+//                 .appName("PortfolioOptimization")
+//                 .master("local[*]")  // 로컬 실행 (클러스터에서는 변경)
+//                 .config("spark.sql.shuffle.partitions", "4") // 설정 추가 가능
+//                 .getOrCreate();
+//     }
+// }
